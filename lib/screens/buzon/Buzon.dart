b87b2a1e249eb1b1ba7c533e94f9a645fc78e6c0
Jose Fernando Resendiz Lopez <56377740+Gerbo67@users.noreply.github.com>
2021-01-28 16:23:06 -0600

    v5.2 beta AppBar de Buzon arreglada

--- lib/screens/buzon/Buzon.dart
+++ lib/screens/buzon/Buzon.dart
@@ -2,48 +2,77 @@ import 'package:flutter/material.dart';
 import 'package:siga_app/models/buzon/mensaje_model.dart';
 import 'package:siga_app/properties/MyColors.dart';
 import 'package:siga_app/properties/MyIcons.dart';
-import 'package:siga_app/screens/home/Home.dart';
-
-import 'AppBarCustom.dart';
+import 'BuzonDialog.dart';
 import 'Mensaje.dart';
 
-Widget Buzon() {
+class Buzon extends StatelessWidget {
+  List<MensajeModel> mensaje = [
+    mensaje1,
+    mensaje2,
+    mensaje3,
+    mensaje1,
+    mensaje2,
+    mensaje3,
+    mensaje1,
+    mensaje2,
+    mensaje3
+  ];
 
-
-  List<MensajeModel> mensaje = [mensaje1,mensaje2,mensaje3,mensaje1,mensaje2,mensaje3,mensaje1,mensaje2,mensaje3];
-
-  return Scaffold(
-      resizeToAvoidBottomPadding: false,
-      body: Builder(
-        builder: (context) {
-          return Column(children: [
-            Expanded(
-                flex: 2,
-                child: Container(
-                  child: Column(
-                    children: [
-                      Expanded(flex: 3, child: Container()),
-                      Expanded(
-                        flex: 7,
-                        child: CustomAppBar(context),
-                      ),
-                    ],
-                  ),
-                )),
-            Expanded(
-              flex: 15,
-              child: Container(
-                //color:Colors.pink,
-                child: ListView(
-                  padding: EdgeInsets.zero,
-                  children: [
-                    for(int i = 0 ; i<mensaje.length;i++)
-                      CustomListTile(mensaje: mensaje[i])
-                  ],
-                ),
-              ),
-            )
-          ]);
-        },
-      ));
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomPadding: false,
+        appBar: AppBar(
+          backgroundColor: LightColor,
+          iconTheme: IconThemeData(color: BlackIcon),
+          actions: [
+            Container(
+                margin: EdgeInsets.all(8),
+                child: Material(
+                  child: InkWell(
+                      onTap: () {
+                        DialogMandarMensaje(context);
+                      },
+                      child: Ink(
+                        width: MediaQuery.of(context).size.width / 2.4,
+                        height: 40,
+                        decoration: BoxDecoration(
+                            color: SecondaryColor,
+                            borderRadius: BorderRadius.all(Radius.circular(3))),
+                        child: Align(
+                            alignment: Alignment.center,
+                            child: Container(
+                              margin: EdgeInsets.all(10),
+                              child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Nuevo Mensaje',
+                                      style: TextStyle(
+                                          color: LightColor, fontSize: 18),
+                                    ),
+                                    Icon(
+                                      IconDesing.mensajes,
+                                      color: LightColor,
+                                      size: 17,
+                                    )
+                                  ]),
+                            )),
+                      )),
+                ))
+          ],
+        ),
+        body: Builder(
+          builder: (context) {
+            return ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                for (int i = 0; i < mensaje.length; i++)
+                  CustomListTile(mensaje: mensaje[i])
+              ],
+            );
+          },
+        ));
+  }
 }
